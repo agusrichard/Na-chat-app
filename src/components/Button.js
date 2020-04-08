a8268e4ad39f138f46b558a487666e11a6871538
agusrichard <agus.richard21@gmail.com>
2020-04-08 19:37:42 +0700

    Create Auth Landing Screen

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,28 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, Text } from 'react-native'
 
-export default function Button() {
+export default function Button(props) {
   return (
-    <TouchableOpacity />
+    <TouchableOpacity 
+      style={styles.buttonContainer}
+      activeOpacity={0.75}
+    >
+      <Text style={styles.buttonText}>{props.title}</Text>
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 300,
+    height: 50,
+    justifyContent: 'center',
+    backgroundColor: 'purple',
+    borderRadius: 10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center'
+  }
+})
--- src/screens/auth/AuthLanding.js
+++ src/screens/auth/AuthLanding.js
@@ -1,37 +1,83 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, ImageBackground } from 'react-native'
+import Button from '../../components/Button'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
-export default class Register extends React.Component {
+export default class AuthLanding extends React.Component {
   render() {
+    const image = require('../../assets/images/auth/authLandingPage.jpg')
+    console.log('image', image)
+
     return (
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Text>Na?</Text>
-          <Text style={styles.quoteText}>“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” – Steve Jobs</Text>
+      <ImageBackground source={image} 
+        style={styles.imageBackground}
+        imageStyle={{ opacity: 0.8 }}
+      >
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.titleText}>Na?</Text>
+            <Text style={styles.quoteText}>“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.”</Text>
+            <Text style={styles.authorText}> – Steve Jobs</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button title="Sign Up" />
+            <View style={styles.signinText}>
+              <Text style={{ color: '#fff' }}>Already have an account?</Text>
+              <TouchableOpacity>
+                <Text style={styles.redirectText}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <Text>Register Button</Text>
-        </View>
-      </View>
+      </ImageBackground>
     )
   }
 }
 
 
 const styles = StyleSheet.create({
+  imageBackground: { 
+    width: '100%', 
+    height: '100%'
+  },
   container: {
     height: '100%',
     width: '100%',
-    padding: 10,
-    justifyContent: "space-around"
+    padding: 25,
+    alignItems: 'center'
   },
   headerContainer: {
-    flex: 2
+    flex: 5
   },
   buttonContainer: {
     flex: 1
   },
+  titleText: {
+    fontSize: 75,
+    marginBottom: 15,
+    color: '#fff',
+    fontFamily: 'Rokkit-ExtraBold',
+    color: '#eb9bd6'
+  },
   quoteText: {
-    fontFamily: 'ComicNeue-Light'
+    fontFamily: 'Rokkit-ExtraLight',
+    fontSize: 20,
+    marginBottom: 10,
+    textAlign: 'justify',
+    color: '#fff'
+  },
+  authorText: {
+    fontFamily: 'ComicNeue-BoldItalic',
+    fontSize: 24,
+    color: '#772978'
+  },
+  signinText: {
+    marginTop: 10,
+    alignSelf: 'center',
+    flexDirection: 'row'
+  },
+  redirectText: {
+    color: '#e3c5e3',
+    marginLeft: 10
   }
 });
