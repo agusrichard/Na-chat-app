8214bca47578767e51a243cf0c635238253ae73d
agusrichard <agus.richard21@gmail.com>
2020-04-10 14:18:52 +0700

    Add maps to maps tab

--- src/screens/main/Maps.js
+++ src/screens/main/Maps.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { View, StyleSheet } from 'react-native'
 
-export default class ChatRoom extends React.Component {
-  render() {
-
-    return (
-      <View>
-        <Text>Maps Tab</Text>
-      </View>
-    )
-  }
+export default function Maps() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
+    </View>
+ );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
